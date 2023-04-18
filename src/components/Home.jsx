39956ad86748b45cfa4e7dac472648213a1b4534
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-04-18 17:22:38 +0530

    css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import HeroImage from "../assets/heroImage.png";
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
-    <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
+    <div name="home" className="h-screen w-full bg-gradient-to-tr from-[#14366F]   to-[#63E5C5]">
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">I'm a Full Stack Developer</h2>
-          <p className="text-gray-500 py-4 max-w-md">
+          <p className="text-white py-4 max-w-md">
             Eu ad magna aliqua commodo duis exercitation eiusmod proident magna.
             Qui irure exercitation ad do ut magna excepteur. Incididunt amet
             nisi ad nisi voluptate dolor sunt exercitation cupidatat fugiat
@@ -18,16 +19,16 @@ const Home = () => {
           </p>
 
             <div>
-                <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+                <Link to="portfolio" smooth duration={500} className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
                     Portfolio
                     <span className="group-hover:rotate-90 duration-300">
                     <MdOutlineKeyboardArrowRight size={25} className="ml-1"/>
                     </span>
-                </button>
+                </Link>
             </div>
         </div>
         <div>
-            <img src={HeroImage} alt="profilePicture" className="rounded-2xl mx-auto w-2/3 md:w-full"/>
+            <img src={HeroImage} alt="profilePicture" className="rounded-3xl mx-auto w-2/3 md:w-full"/>
         </div>
       </div>
     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-scroll";
 
 const NavBar = () => {
   const [nav, setNav] = useState(false);
@@ -28,7 +29,7 @@ const NavBar = () => {
   ];
 
   return (
-    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-[#47466D] fixed">
       <div>
         <h1 className="text-5xl font-signature ml-2">Vivek Anand</h1>
       </div>
@@ -37,9 +38,9 @@ const NavBar = () => {
         {links.map(({ id, link }) => (
           <li
             key={id}
-            className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
+            className="px-4 cursor-pointer capitalize font-medium text-zinc-50 hover:scale-105 duration-200"
           >
-            {link}
+            <Link to={link} smooth duration={500}>{link}</Link>
           </li>
         ))}
       </ul>
@@ -58,7 +59,7 @@ const NavBar = () => {
               key={id}
               className="px-4 cursor-pointer capitalize py-6 text-4xl"
             >
-              {link}
+            <Link onClick={()=>setNav(!nav)} to={link} smooth duration={500}>{link}</Link>
             </li>
           ))}
         </ul>
