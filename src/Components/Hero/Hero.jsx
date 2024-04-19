bfb9492a0583559eae354323853880baeededec4
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2024-04-19 15:18:11 +0530

    Hero Section Completed

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Hero.css'
+import profileImage from '../../assets/profileImage.png'
 
 const Hero = () => {
   return (
-    <div>
-      
+    <div className='hero'>
+      <img src={profileImage} alt='profile image'/>
+      <h1><span>I am Vivek Anand,</span> frontend developer based in India.</h1>
+      <p>I am a frontend developer from India, also have some basic knowledge of the "MERN Stack".</p>
+      <div className='hero-action'>
+        <div className='hero-connect'>Connect with me</div>
+        <div className='hero-resume'>My resume</div>
+      </div>
     </div>
   )
 }
