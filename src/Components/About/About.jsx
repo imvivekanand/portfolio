73fc37f5702e5b65a233414c2c2c1a66dca2c8c9
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2024-04-20 23:30:02 +0530

    Working On About Section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './About.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+import profile_img from '../../assets/profile_img.jpg'
+
 
 const About = () => {
   return (
     <div className='about'>
-      
+      <div className='about-title'>
+        <h1>About me</h1>
+        <img src={theme_pattern} alt=''/>
+      </div>
+      <div className='about-sections'>
+        <div className='about-left'>
+            <img src={profile_img} alt=''/>
+        </div>
+        
+      </div>
     </div>
   )
 }
